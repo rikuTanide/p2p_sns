4ef2cdc8c085f2a97ecd547ffd1c0974d041dbab
riku <riku@tanide.net>
2021-01-14 02:20:36 +0900

    stateを定めた

--- src/ConnectionStatus.ts
+++ src/ConnectionStatus.ts
@@ -1,17 +1,35 @@
 import Peer, { DataConnection } from "skyway-js";
-import { Subject } from "rxjs";
-import { ConnectionBundle } from "./ConnectionBundle";
+// import { Subject } from "rxjs";
+// import { ConnectionBundle } from "./ConnectionBundle";
+import {useEffect, useState} from "react";
 
+export interface State {
+  connectionAuthStatus: ConnectionAuthStatus;
+  users: User[];
+  text: string;
+
+}
+
+export interface User {
+  publicKey: string;
+  publicKeyDigest: string;
+  name: string;
+  introduce: string;
+  trust: boolean;
+  own: boolean;
+  block: boolean;
+  quite: boolean;
+}
 type ComingConnectionStatus =
-  | "connected"
-  | "wait-auth-request"
-  | "processing-auth-request"
-  | "authorized";
+    | "connected"
+    | "wait-auth-request"
+    | "processing-auth-request"
+    | "authorized";
 
 type GoingConnectionStatus =
-  | "connecting"
-  | "processing-auth-request"
-  | "authorized";
+    | "connecting"
+    | "processing-auth-request"
+    | "authorized";
 
 interface GoingConnection {
   status: GoingConnectionStatus;
@@ -31,139 +49,31 @@ interface ValidatedConnection {
   publicKey: string;
   publicKeyHash: string;
 }
+export interface ConnectionAuthStatus {
+   comingConnections: ComingConnection[];
+   goingConnections: GoingConnection[];
+   validatedConnections: ValidatedConnection[];
 
-class ComingConnectionManager {
-  private comingConnections: ComingConnection[] = [];
-}
-
-class GoingConnectionManager {
-  private goingConnections: GoingConnection[] = [];
-}
-
-class ValidatedConnectionManager {
-  private validatedConnections: ValidatedConnection[] = [];
-}
-
-interface OnValidateEvent {
-  connectionID: string;
-  publicKey: string;
-  userName: string;
-  introduce: string;
-}
-
-export interface User {
-  publicKey: string;
-  publicKeyDigest: string;
-  name: string;
-  introduce: string;
-  trust: boolean;
-  own: boolean;
-  block: boolean;
-  quite: boolean;
-}
-
-export class UsersManager {
-  users: User[] = [];
-
-  public readonly doPersistentTrust = new Subject<TrustEvent>();
-  public readonly doPersistentBlock = new Subject<BlockEvent>();
-
-  public readonly onUsers = new Subject<User[]>();
-
-  public onTrust(publicKey: string) {}
-  public onBlock(publicKey: string) {}
-}
-
-export class P2pManager {
-  private comingConnectionManager: ComingConnectionManager;
-  private goingConnectionManager: GoingConnectionManager;
-  private validatedConnectionManager: ValidatedConnectionManager;
-
-  public readonly onConnectionStatusUpdate = new Subject<
-    ValidatedConnection[]
-  >();
-  public readonly onUserProfileUpdate = new Subject<ValidatedConnection[]>();
-
-  public readonly onQuiteEvent = new Subject<string>();
-
-  public readonly onChatEventLog = new Subject<ChatEventLog>();
-
-  public constructor() {
-    this.comingConnectionManager = new ComingConnectionManager();
-    this.goingConnectionManager = new GoingConnectionManager();
-    this.validatedConnectionManager = new ValidatedConnectionManager();
-
-    this.comingConnectionManager.onValidate.subscribe(
-      (event: OnValidateEvent) => this.onValidateComingConnection(event)
-    );
-    this.goingConnectionManager.onValidate.subscribe((event: OnValidateEvent) =>
-      this.onValidateGoingConnection(event)
-    );
-
-    this.comingConnectionManager.doConnectionClose((connectionID) =>
-      this.doConnectionClose.next(connectionID)
-    );
-    this.goingConnectionManager.doConnectionClose((connectionID) =>
-      this.doConnectionClose.next(connectionID)
-    );
-    this.validatedConnectionManager.doConnectionClose((connectionID) =>
-      this.doConnectionClose.next(connectionID)
-    );
-
-    this.comingConnectionManager.doSendMessage((event: OnRequestMessageEvent) =>
-      this.doSendMessage(event)
-    );
-    this.goingConnectionManager.doSendMessage((event: OnRequestMessageEvent) =>
-      this.doSendMessage(event)
-    );
-    this.validatedConnectionManager.doSendMessage(
-      (event: OnRequestMessageEvent) => this.doSendMessage(event)
-    );
-
-    this.validatedConnectionManager.onQuiteEvent.subscribe((publicKey) =>
-      this.onQuiteEvent.next(publicKey)
-    );
-    this.validatedConnectionManager.onComment.subscribe((comment) =>
-      this.onComment.next(publicKey)
-    );
-    this.validatedConnectionManager.onProfileUpdate.subscribe((profile) =>
-      this.onProfileUpdate.next(publicKey)
-    );
-    this.validatedConnectionManager.onAssets.subscribe((profile) =>
-      this.onAssets.next(publicKey)
-    );
-  }
-
-  public onMessage(connectionID: string, data: any[]) {
-    const connectionType = this.findConnectionType(connectionID);
-
-    if (connectionType == "coming") {
-      this.comingConnectionManager.onMessage(data);
-    } else if (connectionType == "going") {
-      this.goingConnectionManager.onMessage(data);
-    } else if (connectionType == "validated") {
-      this.validatedConnectionManager.onMessage(data);
-    }
-  }
-
-  public connect(remoteID: string) {}
-
-  public sendComment(remoteIDs: string[], data: string) {}
 }
 
 const skyWayKey = "77157c8d-8852-4dd0-b465-10f57625ffc7";
 
-export async function setUp() {
-  const ps = new PersistentService();
-  const name = await ps.name();
-  const ownKeyManager = await OwnKeyManager.get(ps);
-  const cb = await ConnectionBundle.open(skyWayKey);
-  const p2p = new P2pManager(ownKeyManager);
+const defaultState: State = {
+  connectionAuthStatus: {
+    comingConnections:[],
+    goingConnections: [],
+    validatedConnections: [],
+  },
+  users: [],
+  text: "",
+}
 
-  const u = new UrlParser();
+interface Handlers {
 
-  if (u.hasToId()) {
-    p2p.connect(u.getToId(), name);
-  }
+}
 
+function useSns() {
+  const [state, setState] = useState<State>(defaultState);
+  useEffect(() => {});
+  return state;
 }
